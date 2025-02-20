95e8444128ec0ee132cabbd3cc3655810a04bfa7
Данила Андреев <danila00009@gmail.com>
2025-02-20 18:06:45 +0300

    add controlles to ger services all and by id
    Signed-off-by: Данила Андреев <danila00009@gmail.com>

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -1,8 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ServiceManager.Data;
 
-namespace ServiceManager.Controllers;
-
+namespace ServiceManager.Controllers
+{
+    [Route("api/Service")]
+    [ApiController]
 public class ServiceController : ControllerBase
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public ServiceController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetService()
+    {
+        var services = _context.Services.ToList();
+        return Ok(services);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult GetServiceById([FromRoute] Guid id)
+    {
+        var services = _context.Services.Find(id);
+        
+        if(services == null)
+            return NotFound();
+        return Ok(services);
+    }
+}
 }
